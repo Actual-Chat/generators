001ec454211e08a6c2182c9286c1ccd8e960b545
Alex Yakunin <alex.yakunin@gmail.com>
2022-06-09 01:25:20 -0700

    fix: pack & publish

--- build/Pack.cmd
+++ build/Pack.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd ..
 set PUBLIC_BUILD=1
-rmdir /S /Q artifacts 2>nul
+rmdir /S /Q artifacts/nupkg 2>nul
 dotnet pack -c:Release
 popd
--- build/Publish.cmd
+++ build/Publish.cmd
@@ -1,16 +1,5 @@
 @echo off
-pushd ..
-set PUBLIC_BUILD=1
-rmdir /S /Q artifacts 2>nul
-dotnet pack -c:Release
-
-pushd artifacts\nupkg
-call :publish ActualLab.Generators.nupkg
-call :publish ActualLab.Generators.Abstractions.nupkg
+call Pack.cmd
+pushd ..\artifacts\nupkg
+dotnet nuget push ActualLab.Generators.*.nupkg -k %ACTUALCHAT_NUGET_API_KEY% -s https://api.nuget.org/v3/index.json
 popd
-popd
-goto :eof
-
-:publish
-dotnet nuget push %1 -k %ACTUALCHAT_NUGET_API_KEY% -s https://api.nuget.org/v3/index.json
-goto :eof
